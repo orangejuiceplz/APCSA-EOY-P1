24d7424f2723d0b6f6137d93488ca8ec0f8ba1e9
LL-LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2025-05-12 19:30:22 -0400

    basic

--- src/main/java/com/dl_labs/chatroom/user_stuff/person.java
+++ src/main/java/com/dl_labs/chatroom/user_stuff/person.java
@@ -5,5 +5,9 @@ public class Person {
     private String name;
     private boolean isHost;
 
+    public Person(String name, boolean isHost) {
+        this.name = name;
+        this.isHost = isHost;
+    }
 
 }
