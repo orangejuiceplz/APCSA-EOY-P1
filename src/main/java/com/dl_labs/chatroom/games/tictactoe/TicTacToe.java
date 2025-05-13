abc84eef79ec5d1c503cbdd490ac8092d4fdb576
λamdashi <155986030+orangejuiceplz@users.noreply.github.com>
2025-05-13 13:01:51 +0000

    ok
    Co-authored-by: NoGambling <NoGambling1@users.noreply.github.com>

---
+++ src/main/java/com/dl_labs/chatroom/games/tictactoe/TicTacToe.java
@@ -0,0 +1,12 @@
+package com.dl_labs.chatroom.games.tictacttoe;
+
+
+public class TicTacToe {
+    private String p1Char;
+    private Sring p2Char;    
+    
+    public TicTacToe(String player1Symbol, String player2Symbol) {
+        p1Char = player1Symbol;
+        p2Char = player2Symbol;
+            }
+}
