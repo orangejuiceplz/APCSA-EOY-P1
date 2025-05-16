962d80ceae31b9c874f69ee3d581f7c338b4e8d6
NoGambling <110059421+NoGambling1@users.noreply.github.com>
2025-05-16 12:43:21 -0400

    Update TicTacToe.java
    added commends for future plannign

--- temp/TicTacToe.java
+++ temp/TicTacToe.java
@@ -7,6 +7,7 @@ public class TicTacToe {
     private char p2Char;    
     private char[][] board = new char[3][3];
     private char currentPlayer;
+    
 
     private ArrayList<String[][]> winningComboes = new ArrayList<>();
     
@@ -97,3 +98,43 @@ public class TicTacToe {
     
 }
  
+/* for the future 
+public class ColoredConsole {
+    // Reset
+    public static final String RESET = "\033[0m";
+
+    // Regular Colors
+    public static final String BLACK = "\033[0;30m";   
+    public static final String RED = "\033[0;31m";     
+    public static final String GREEN = "\033[0;32m";   
+    public static final String YELLOW = "\033[0;33m";  
+    public static final String BLUE = "\033[0;34m";    
+    public static final String PURPLE = "\033[0;35m";  
+    public static final String CYAN = "\033[0;36m";    
+    public static final String WHITE = "\033[0;37m";   
+    
+    }
+}
+
+final String firstCharColor = RED;
+final String secondCharColor = BLACK;
+
+        System.out.println("[1 2 3] |  [" + board[0][0] + " " + board[0][1] + " " + board[0][2] + " ]");
+        System.out.println("[4 5 6] |  [" + board[1][0] + " " + board[1][1] + " " + board[1][2] + " ]");
+        System.out.println("[7 8 9] |  [" + board[2][0] + " " + board[2][1] + " " + board[2][2] + " ]");
+
+ System.out.print("[1 2 3] |  ["
+ for (int e = 0; e < 3; e++) {
+  if (board[0][e] == firstChar) { 
+    System.out.print(RED + board[0][e] + RESET);
+  } else if (board[0][e] == secondChar) {
+    System.out.print(BLACK + board[0][e] + RESET); 
+  } else {
+    System.out.print("_");
+ }
+  System.out.print(" ");
+}
+System.out.print(" ]");
+
+
+*/
